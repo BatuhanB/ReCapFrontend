807c8b87ab69901349aef6db0d9c7f1e03c8b523
BatuhanB <balibatuhan19@gmail.com>
2022-05-27 10:19:05 +0300

    Routing has been added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { CustomerComponent } from './components/customer/customer.component';
+import { ColorComponent } from './components/color/color.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { CarComponent } from './components/car/car.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",pathMatch:"full",component:CarComponent},
+  {path:"cars",component:CarComponent},
+  {path:"brands",component:BrandComponent},
+  {path:"rentals",component:RentalComponent},
+  {path:"colors",component:ColorComponent},
+  {path:"customers",component:CustomerComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
